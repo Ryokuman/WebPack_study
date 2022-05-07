2995572dc148b835e96bafdce0b06df4b144f5e3
Ryokuman <ryokuman@naver.com>
2022-05-07 17:55:12 +0900

    MOD: webpack.config.js { MOD: entry , output } ADD: about.html , source/about.js

---
+++ source/about.js
@@ -0,0 +1,5 @@
+import Hello_word from "./hello.js";
+import World_word from "./world.js";
+import css from "./style.css";
+
+document.getElementById("root").innerHTML = World_word + " " + Hello_word;
